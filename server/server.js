1492aa0c895d90d9d7ba6ba09cb253d9b1a6e1c4
MeraryMC <m_martinez_cobian@berkeley.edu>
2019-02-06 20:27:33 -0800

    database seeded and we are logging banner info to the console

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const {Banner, db, save} = require('../database/banner_db');
 
 let app = express();
 let port = 8080;
@@ -8,6 +9,18 @@ let port = 8080;
 app.use(bodyParser.json({type: 'application/json'}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../')));
+
+
+app.get('/api/movies/banner', function (req, res) {
+   Banner.find().exec()
+   .then(results => {
+    if (!results){
+      return res.status(500).send({})
+    }
+    console.log(results);
+    return res.status(200).send(results);
+  })
+});
 
 app.listen(port, (error) => {
   if (error) {
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import $ from 'jquery';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      bannerData:[]
+    }
+
+   this.getBannerData = this.getBannerData.bind(this);
+  }
+
+  componentDidMount(){
+    this.getBannerData();
+  }
+
+  getBannerData() {
+    var that = this;
+    $.ajax({
+      url: 'http://localhost:8080/api/movies/banner',
+      method: 'GET',
+      success: function(results) {
+        that.setState({
+          bannerData:results
+        });
+        console.log(results)
+      },
+      error: () => console.log('Error in getBannerData')
+    });
+
   }
 
   render() {
