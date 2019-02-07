d9a64ff7cfca2b9895e5dfa210f87e620338e867
MeraryMC <m_martinez_cobian@berkeley.edu>
2019-02-07 12:38:20 -0800

    updates to db and added jest to package.json

--- database/seed.js
+++ database/seed.js
@@ -2,25 +2,13 @@ const {db, Banner} = require('./banner_db.js');
 
 var bannerData = [
   {
+
     "video": "https://www.youtube.com/watch?v=lc0UehYemQA",
     "title": "Jurassic Park",
     "year" : "1993",
-    "score_title" : "Metascore",
-    "question" : "placeholder for ?",
     "score_explanation" : "Generally favorable reviews based on 20 Critics",
     "score" : "68",
-    "see_all" : "See All",
-    "vote_now" : "Vote Now",
-    "1" : "1",
-    "2" : "2",
-    "3" : "3",
-    "4" : "4",
-    "5" : "5",
-    "6" : "6",
-    "7" : "7",
-    "8" : "8",
-    "9" : "9",
-    "10" : "10"
+    "scores_obj" : {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
   }
 ];
 
